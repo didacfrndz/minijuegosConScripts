8692fb36caf53902bacfb41b85f2a10890b0503e
didacfrndz <fernandezbarcodidac@fpllefia.com>
2025-11-05 15:46:55 +0100

    JuegoDeLaVidaDentro

---
+++ minijuegoJuegoDeLaVida/script/patrones.js
@@ -0,0 +1,23 @@
+
+const glider = [
+    [false, true, false],
+    [false, false, true],
+    [true, true, true]
+];
+
+const blinker = [
+    [true, true, true]
+];
+
+const toad = [
+    [false, true, true, true],
+    [true, true, true, false]
+];
+
+const exploder = [
+    [false, true, false, true, false],
+    [true, true, true, true, true],
+    [false, true, false, true, false]
+];
+
+export { glider, blinker, toad, exploder };
