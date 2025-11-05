0be12de7d78e254a7893dc3892b6424d130b68d7
didacfrndz <fernandezbarcodidac@fpllefia.com>
2025-11-05 18:08:56 +0100

    PacmanImplementado

---
+++ minijuegoPadman/scripts/enemy.js
@@ -0,0 +1,7 @@
+export const enemigos = [
+    { f: Math.floor(Math.random() * 16), c: Math.floor(Math.random() * 15), vivo: true },
+    { f: Math.floor(Math.random() * 16), c: Math.floor(Math.random() * 15), vivo: true },
+    { f: Math.floor(Math.random() * 16), c: Math.floor(Math.random() * 15), vivo: true },
+    { f: Math.floor(Math.random() * 16), c: Math.floor(Math.random() * 15), vivo: true },
+    { f: Math.floor(Math.random() * 16), c: Math.floor(Math.random() * 15), vivo: true }
+];
